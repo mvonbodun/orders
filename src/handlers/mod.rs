b196a64078f8f241ea2d522d8c58254dee482ae2
Michael von Bodungen <michael@vonbodungen.com>
2024-08-07 21:13:22 -0400

    Working version with Axum

---
+++ src/handlers/mod.rs
@@ -0,0 +1,56 @@
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
+
+use crate::{model::OrderCreateRequest, AppState};
+
+mod handlers_inner;
+
+impl IntoResponse for handlers_inner::HandlerError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            handlers_inner::HandlerError::BadRequest(message) => {
+                (StatusCode::BAD_REQUEST, message).into_response()
+            }
+            handlers_inner::HandlerError::InternalError(message) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, message).into_response()
+            }
+        }
+    }
+}
+
+// Create order
+pub async fn create_order(
+    State(AppState { orders_dao }): State<AppState>,
+    Json(order_create_request): Json<OrderCreateRequest>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::create_order(order_create_request, orders_dao.as_ref())
+        .await
+        .map(Json)
+}
+
+// Get order
+pub async fn get_order(
+    State(AppState { orders_dao }): State<AppState>,
+    Path(id): Path<String>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let result = handlers_inner::get_order(id, orders_dao.as_ref()).await;
+    match result {
+        Ok(Some(order)) => Ok(Json(order)),
+        Ok(None) => Err((StatusCode::NOT_FOUND, "Order not found").into_response()),
+        Err(e) => Err(e.into_response()),
+    }
+}
+
+// Delete order
+pub async fn delete_order(
+    State(AppState { orders_dao }): State<AppState>,
+    order_id: String,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::delete_order(order_id, orders_dao.as_ref())
+        .await
+        .map(Json)
+}
